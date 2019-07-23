baa2b194f015ae1e2975c1c07cce808da158bd79
trungtv4597 <trungtv4597@gmail.com>
2019-07-24 01:12:22 +0700

    Initial commit

---
+++ chapter1.py
@@ -0,0 +1,27 @@
+# 1: Viêt một câu với đúng ngữ nghĩa nhưng sai cú pháp.
+#   Viết một câu khác đùng cú pháp nhưng lại sai ngữ nghĩa.
+
+# 2: Dùng Interpreter, nhập các biểu thức (expression) với các dấu +, *, ** và cho chạy.
+# Python tính toán các biểu thức này và biểu thị kết quả ngay ở dòng phía dưới.
+
+# 3: Nhập 1 2 và cho chạy.
+# Báo lỗi sai cú pháp.
+
+# 4: Nhập vào Interpreter "hello" và print("hello"), sau đó miêu tả kết quả và ghi chú khi nào dấu nháy kép xuất hiện, khi nào không. 
+#   Nhập print("hello"). Kết quả là in ra các ký tự h-e-l-l-o
+#   Nhập "hello". Kết quả in ra: "hello".
+#   Không có nháy kép (quotation marks) khi dùng hàm print.
+
+# 5: Nhập cheese mà không có dấu nháy kép thì khi chạy chương trình sẽ báo lỗi runtime, 
+# mà cụ thể là NameError (lỗi tên) tại vị cheese chưa khai báo.
+
+# 6: Nhập và 6 + 4 * 9 tại prompt và xem chuyện j xảy ra. 
+# Kết quả là 42.
+#   Bây giờ mập tạo một script với một dung: 6 + 4 * 9. 
+# Khi chạy script thì không có kết quả nào dc xuất ra.
+#   Thay đổi mội dung script thành: print(6 + 4 * 9). 
+# Khi chạy thì kết quả là 42.
+#   Chạy script lần nữa. 
+# Khi chạy thì kết quả vẫn là 42.   
+#   KẾT LUẬN: Mỗi khi một biểu thức (expression) được nhập vào Python prompt, 
+# nó sẽ tính toán và kết quả tự động xuất hiện ở dòng dưới. Một script thì khác, nó cần hàm print để xuất kết quả ra.
